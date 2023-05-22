ff7935204ca03b28e63088ad36692e9dc41c1de3
Rajeev Krishna <lambo2572@gmail.com>
2023-05-22 13:07:05 +0530

    Main repo updated

--- portfolio/src/app/about.js
+++ portfolio/src/app/about.js
@@ -1,5 +1,12 @@
+import Image from "next/image"
+
 function About(){
-    return <h1>About page</h1>
+    return <div className="flex">
+        <div className="">
+            <img src="" alt="" />
+        </div>
+        <h1 className="text-center max-w-md">Hey this is Rajeev, A frontend developer from Indore and a pre-final year student. I have keen knowledge in web technologies like HTML,CSS and other </h1>
+    </div>
 }
 
 export default About
