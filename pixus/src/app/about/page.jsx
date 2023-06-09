515bd6de7a251bf70af0384dd6a7c46a7ae05284
Rajeev Krishna <lambo2572@gmail.com>
2023-06-09 21:28:12 +0530

    Added styling to the home page and started for the rest

--- pixus/src/app/about/page.jsx
+++ pixus/src/app/about/page.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import styles from "./page.module.css"
+import Image from "next/image"
 
 const About = () => {
     return (
-        <div className={styles.container}>About</div>
+        <div className={styles.container}>
+            <div className={styles.imgContainer}>
+                <Image src= fill={true} alt=""/>
+            </div>
+            <div className={styles.textContainer}></div>
+        </div>
     )
 }
 
--- pixus/src/app/page.js
+++ pixus/src/app/page.js
@@ -1,12 +1,24 @@
 // import Image from 'next/image'
 import Link from 'next/link'
 import styles from './page.module.css'
+import Image from 'next/image'
 import Hero from 'public/hero.png'
 
 export default function Home() {
   return (
-    <div className="">
-      <Image src={Hero} alt="hero"/>
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          The place for designing better products
+        </h1>
+        <p className={styles.desc}>
+          I design and develop the designs to bring it live to life.
+        </p>
+        <button className={styles.button}>My projects</button>
+      </div>
+      <div className={styles.item}>
+      <Image src={Hero} alt="hero" className={styles.image}/>
+      </div>
     </div>
   )
 }
